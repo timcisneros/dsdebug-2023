7334249d99a1bc4df92d06915e1fa79a4660f8dd
tcstria <tcisneros@stria.com>
2023-10-05 23:08:22 -0700

    updated config formatting

--- next.config.js
+++ next.config.js
@@ -3,15 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: process.env.NEXT_PUBLIC_IMAGE_URL,
-                port: '',
-                // pathname: '/account123/**',
-            },
-        ],
-        domains: ['localhost:3000'],
+        domains: ['localhost:3000', process.env.NEXT_PUBLIC_IMAGE_URL],
     },
     webpack(config) {
         config.module.rules.push({
